652489e155048ecb18f09821ba4076ce85421a7e
Hernando Abella <hernandoabella@gmail.com>
2022-11-19 09:37:50 -0500

    agregando slider

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 // dark mode btn
 
-let icon = document.querySelector('i.fa-moon-o')
-let btn = document.querySelector('div.dark-mode-btn')
-let body = document.querySelector('body');
-
-
-// icon.addEventListener('click', function(){
-//     body.classList.toggle('dark-mode')
-//     this.classList.toggle('backgroundBtn')
-//     this.classList.toggle('fa-sun-o')
-// })
-
-// function myFunction(x) {
-//     x.classList.toggle('fa-thumbs-down')
-// }
 
 // show datetime
 function myFunction2() {
@@ -24,3 +10,24 @@ function myFunction2() {
 
 setInterval(myFunction2, 1000)
 
+// testimonial slideshow 
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
