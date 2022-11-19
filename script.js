4fed148f6caa139d9a06ff2cc9c6f2e66975643f
Hernando Abella <hernandoabella@gmail.com>
2022-11-18 20:12:44 -0500

    hero page complete

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// dark mode
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+// dark mode btn
+
+let icon = document.querySelector('i.fa-duotone.fa-moon')
+let btn = document.querySelector('div.dark-mode-btn')
+let body = document.querySelector('body');
+let anchor = document.querySelector('header nav ul li a');
+
+btn.addEventListener('click', function(){
+    body.classList.toggle('dark-mode')
+    icon.classList.toggle('fa-duotone.fa-sun')
+    this.classList.toggle('backgroundBtn')
+    anchor.classList.toggle('dark-mode');
+})
+
+// show datetime
+function myFunction2() {
+    var date = new Date()
+    var current_time = date.getHours() + ":" + date.getMinutes()
+    document.getElementById('hora').innerHTML = current_time; 
 }
+
+setInterval(myFunction2, 1000)
