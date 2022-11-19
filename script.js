9abeb70ab15d5a1b824ff164a9cf9faed5b9c2de
Hernando Abella <hernandoabella@gmail.com>
2022-11-18 20:43:32 -0500

    cambios
    cambios de intro a hero

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // dark mode btn
 
-let icon = document.querySelector('i.fa-duotone.fa-moon')
+let icon = document.querySelector('i.fa-moon')
 let btn = document.querySelector('div.dark-mode-btn')
 let body = document.querySelector('body');
-let anchor = document.querySelector('header nav ul li a');
+let anchor = document.querySelectorAll('header nav ul li > a');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function(x){
     body.classList.toggle('dark-mode')
-    icon.classList.toggle('fa-duotone.fa-sun')
     this.classList.toggle('backgroundBtn')
-    anchor.classList.toggle('dark-mode');
 })
 
 // show datetime
