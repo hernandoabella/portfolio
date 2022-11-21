c13eb7b42bc33db8dc005c83bd2312014cefb6b2
Hernando Abella <hernandoabella@gmail.com>
2022-11-21 18:26:43 -0500

    agregando caracteristica dark mode

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // dark mode btn
+function darkMode() {
+    let body = document.getElementById('body');
+    body.classList.toggle('dark-mode');
+}
 
 
 // show datetime
 function myFunction2() {
     var date = new Date()
-    var current_time = date.getHours() + ":" + date.getMinutes()
+    var current_time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
     document.getElementById('hora').innerHTML = current_time; 
 }
 
@@ -30,3 +34,25 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
+
+// work section
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" activo", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " activo";
+}
+
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+
+
+
+// contact form 
