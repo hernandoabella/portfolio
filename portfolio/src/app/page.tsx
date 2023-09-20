85d2467f69a065eb31ae65751a787f04d9cdc2db
Hernando Abella <hernandoabella@gmail.com>
2023-09-20 16:12:44 -0500

    updates

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import WorkExperience from '../components/WorkExperience';
-import Toolbox from '../components/Toolbox';
-import MyWorks from '../components/MyWorks';
-import Testimonials from '../components/Testimonials';
-import CTA from '../components/CTA';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
+import Header from '../app/components/Header';
+import Hero from '../app/components/Hero';
+import WorkExperience from '../app/components/WorkExperience';
+import Toolbox from '../app/components/Toolbox';
+import MyWorks from '../app/components/MyWorks';
+import Testimonials from '../app/components/Testimonials';
+import CTA from '../app/components/CTA';
+import Contact from '../app/components/Contact';
+import Footer from '../app/components/Footer';
 
 export default function Home() {
   return (
