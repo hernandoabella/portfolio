4351c61641ec1aa475bf3090e1008fe476174f93
Hernando Abella <hernandoabella@gmail.com>
2023-09-20 15:47:56 -0500

    updates

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,17 +1,27 @@
 // src/components/Header.tsx
-import React from 'react';
+import React from "react";
 
 const Header: React.FC = () => {
   return (
     <header className="bg-blue-500 text-white p-4">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-semibold">My Portfolio</h1>
-        <nav className="mt-2">
+      <div className="container mx-auto flex items-center justify-between">
+        <div>
+          <h1>Hernando Abella</h1>
+        </div>
+        <nav>
           <ul className="flex space-x-4">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Portfolio</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
           </ul>
         </nav>
       </div>
--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import Image from "next/image";
 
 const Hero: React.FC = () => {
   return (
     <section className="bg-blue-900 text-white py-20">
       <div className="container mx-auto text-center">
-        <h1 className="text-4xl font-semibold mb-4">Welcome to My Portfolio</h1>
-        <p className="text-lg mb-8">Showcasing my skills and projects in the world of development.</p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300">View Projects</button>
+        <Image
+          src="/profile.png"
+          alt="Hernando Abella"
+          width={128} // Define el ancho de la imagen
+          height={128} // Define la altura de la imagen
+          className="mx-auto rounded-full w-32 h-32 mb-4"
+        />
+        <h1 className="text-4xl font-semibold mb-4">Hernando Abella</h1>
+        <p className="text-lg mb-8">Full-Stack Engineer</p>
+        <p className="text-lg mb-8">
+          I'm a dynamic Full-Stack engineer with a passion for crafting
+          exceptional solutions. Let's redefine what's possible together!
+        </p>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300">
+          View Projects
+        </button>
       </div>
     </section>
   );
