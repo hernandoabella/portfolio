a0aece5f0f24a4a8a53134c897a5b4ced9fcfed2
Hernando Abella <hernandoabella@gmail.com>
2023-09-20 22:19:46 -0500

    Update Testimonials.tsx

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -8,19 +8,19 @@ const Testimonials: React.FC = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Testimonio 1 */}
           <div className="bg-white p-4 rounded-lg shadow">
-            <p className="text-gray-600">"I had a great experience working with [Your Name]. Their expertise and professionalism made our project a success."</p>
+            <p className="text-gray-600">&ldquo;I had a great experience working with [Your Name]. Their expertise and professionalism made our project a success.&rdquo;</p>
             <p className="font-semibold">John Doe</p>
           </div>
 
           {/* Testimonio 2 */}
           <div className="bg-white p-4 rounded-lg shadow">
-            <p className="text-gray-600">"I highly recommend [Your Name] for their exceptional development skills. They delivered on time and exceeded our expectations."</p>
+            <p className="text-gray-600">&ldquo;I highly recommend [Your Name] for their exceptional development skills. They delivered on time and exceeded our expectations.&rdquo;</p>
             <p className="font-semibold">Jane Smith</p>
           </div>
 
           {/* Testimonio 3 */}
           <div className="bg-white p-4 rounded-lg shadow">
-            <p className="text-gray-600">"Working with [Your Name] was a pleasure. They are highly skilled and communication was excellent throughout the project."</p>
+            <p className="text-gray-600">&ldquo;Working with [Your Name] was a pleasure. They are highly skilled and communication was excellent throughout the project.&rdquo;</p>
             <p className="font-semibold">Michael Johnson</p>
           </div>
 
