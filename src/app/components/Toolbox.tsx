4ba523c2c101d4fc6b43766e9e5a7948bd5cbd5e
Hernando Abella <hernandoabella@gmail.com>
2023-09-22 21:15:17 -0500

    Update Toolbox.tsx

--- src/app/components/Toolbox.tsx
+++ src/app/components/Toolbox.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 
+
+// ... importa las demás imágenes ...
+
 const Toolbox: React.FC = () => {
   return (
     <section className="bg-gray-100 py-10">
       <div className="container mx-auto">
         <h2 className="text-2xl font-semibold mb-4">Toolbox</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Habilidad/Herramienta 1 */}
+          {/* HTML */}
           <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="text-lg font-semibold">Skill/Tool 1</h3>
-            <p className="text-gray-600">Description of your skill or tool. You can mention your proficiency level and any relevant details.</p>
+            
+            <h3 className="text-lg font-semibold">HTML5</h3>
+            <p className="text-gray-600">Description of your HTML5 proficiency and any relevant details.</p>
           </div>
 
-          {/* Habilidad/Herramienta 2 */}
+          {/* CSS */}
           <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="text-lg font-semibold">Skill/Tool 2</h3>
-            <p className="text-gray-600">Description of your skill or tool. You can mention your proficiency level and any relevant details.</p>
+            
+            <h3 className="text-lg font-semibold">CSS3</h3>
+            <p className="text-gray-600">Description of your CSS3 proficiency and any relevant details.</p>
           </div>
 
-          {/* Habilidad/Herramienta 3 */}
-          <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="text-lg font-semibold">Skill/Tool 3</h3>
-            <p className="text-gray-600">Description of your skill or tool. You can mention your proficiency level and any relevant details.</p>
-          </div>
-
-          {/* Agrega más habilidades/herramientas según sea necesario */}
+          {/* Otras habilidades/herramientas */}
+          {/* Sigue el mismo patrón para las demás habilidades/herramientas */}
         </div>
       </div>
     </section>
