060e8c198eddde2ef62d11486d4226e42e05be64
Hernando Abella <hernandoabella@gmail.com>
2023-09-22 22:07:59 -0500

    Update Testimonials.tsx

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import Image from "next/image";
+
 const Testimonials: React.FC = () => {
   return (
     <section className="bg-gray-100 py-10">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-semibold mb-8 text-center mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight dark:text-slate-50 ">
-          Clients Testimonials ⭐
+        <h2 className="text-3xl font-semibold mb-8 text-center mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight dark:text-slate-50">
+          Client Testimonials ⭐
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {/* Testimonio 1 */}
+          {/* Testimonial 1 */}
           <div className="text-sm leading-6">
             <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
               <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
                 <p>
-                  He is an empathetic person who cares deeply about the results
-                  of his clients and always gives an incredible service. I
-                  recommend it 100% to every person who needs digital services.
+                <i className="fa-solid fa-quote-left"></i> He is an empathetic person who cares deeply about the results of his clients and always provides an incredible service. I recommend him 100% to anyone in need of digital services. <i className="fa-solid fa-quote-right"></i>
                 </p>
               </blockquote>
               <figcaption className="flex items-center space-x-4">
@@ -33,8 +32,8 @@ const Testimonials: React.FC = () => {
                 <div className="flex-auto">
                   <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
                     <a href="https://twitter.com/brethart" target="_blank">
-                      <span className="absolute inset-0"></span>Bret "The
-                      Hitman" Hart
+                      <span className="absolute inset-0"></span>Bret &quot;The
+                      Hitman&quot; Hart
                     </a>
                   </div>
                   <div className="mt-0.5">Former WWE Champion</div>
@@ -50,17 +49,12 @@ const Testimonials: React.FC = () => {
             </figure>
           </div>
 
-          {/* Testimonio 2 */}
+          {/* Testimonial 2 */}
           <div className="text-sm leading-6">
             <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
               <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
                 <p>
-                  One of the things that I highlight the most is the versatility
-                  it has to be able to capture what the client really needs and
-                  what the client wants to project and make known on their
-                  digital pages. He is a person who really works for you with
-                  love, dedication, commitment, and responsibility, there is
-                  never no for an answer.
+                <i className="fa-solid fa-quote-left"></i> One of the things I appreciate the most is its versatility in capturing what the client truly needs and what the client wants to convey and promote on their digital platforms. He is someone who works for you with love, dedication, commitment, and responsibility, and there is never a &quot;no&quot; for an answer. <i className="fa-solid fa-quote-right"></i>
                 </p>
               </blockquote>
               <figcaption className="flex items-center space-x-4">
@@ -94,14 +88,14 @@ const Testimonials: React.FC = () => {
             </figure>
           </div>
 
-          {/* Testimonio 3 */}
+          {/* Testimonial 3 */}
           <div className="text-sm leading-6">
             <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
               <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
                 <p>
-                  We couldn't have made a better decision. Hernando has a great
+                <i className="fa-solid fa-quote-left"></i> We couldn&apos;t have made a better decision. Hernando has a great
                   human team: competent, responsible, and committed. The results
-                  show the quality and experience of his work.
+                  show the quality and experience of his work. <i className="fa-solid fa-quote-right"></i>
                 </p>
               </blockquote>
               <figcaption className="flex items-center space-x-4">
