6860b72d4cfb21ecaae51a7d8a5c759f9736aad0
Hernando Abella <hernandoabella@gmail.com>
2023-09-22 21:15:10 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import Header from '../app/components/Header';
-import Hero from '../app/components/Hero';
-import WorkExperience from '../app/components/WorkExperience';
-import Toolbox from '../app/components/Toolbox';
-import MyWorks from '../app/components/MyWorks';
-import Testimonials from '../app/components/Testimonials';
-import CTA from '../app/components/CTA';
-import Contact from '../app/components/Contact';
-import Footer from '../app/components/Footer';
+import React from "react";
+import Header from "../app/components/Header";
+import Hero from "../app/components/Hero";
+import WorkExperience from "../app/components/WorkExperience";
+import Toolbox from "../app/components/Toolbox";
+import MyWorks from "../app/components/MyWorks";
+import Testimonials from "../app/components/Testimonials";
+import CTA from "../app/components/CTA";
+import Contact from "../app/components/Contact";
+import Footer from "../app/components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Header />
-      <Hero />
-      <WorkExperience />
-      <Toolbox />
-      <MyWorks />
-      <Testimonials />
-      <CTA />
-      <Contact />
-      <Footer />
+      <div>
+        <Header />
+        <Hero />
+        <WorkExperience />
+        <Toolbox />
+        <MyWorks />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
