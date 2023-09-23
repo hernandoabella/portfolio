9f83cc4a975979f403b16a341167025e3cddb101
Hernando Abella <hernandoabella@gmail.com>
2023-09-22 22:07:57 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import WorkExperience from "../app/components/WorkExperience";
 import Toolbox from "../app/components/Toolbox";
 import MyWorks from "../app/components/MyWorks";
 import Testimonials from "../app/components/Testimonials";
+import Skills from "../app/components/Skills";
+import Services from "../app/components/Services";
 import CTA from "../app/components/CTA";
 import Contact from "../app/components/Contact";
 import Footer from "../app/components/Footer";
@@ -19,6 +21,30 @@ export default function Home() {
         <Toolbox />
         <MyWorks />
         <Testimonials />
+        <Skills />
+        <Services
+          services={[
+            {
+              id: 1,
+              title: "Desarrollo web",
+              description:
+                "Creamos sitios web a medida para satisfacer tus necesidades.",
+            },
+            {
+              id: 2,
+              title: "Diseño gráfico",
+              description:
+                "Diseñamos gráficos y logotipos atractivos y únicos.",
+            },
+            {
+              id: 3,
+              title: "Consultoría de proyectos",
+              description:
+                "Brindamos asesoramiento y gestión de proyectos tecnológicos.",
+            },
+          ]}
+        />
+        <CTA />
         <Contact />
         <Footer />
       </div>
