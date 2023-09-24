4c7b2f5e03805e5a8e8bb6cfbbf531331cf00d71
Hernando Abella <hernandoabella@gmail.com>
2023-09-23 19:37:19 -0500

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const Hero: React.FC = () => {
   return (
-    <section className="bg-blue-900 text-white py-20">
-      <div className="container mx-auto text-center">
+    <section className="bg-white  py-20">
+      <div className="container mx-auto text-center ">
         <Image
           src="/profile.png"
           alt="Hernando Abella"
-          width={128} // Define el ancho de la imagen
-          height={128} // Define la altura de la imagen
+          width={128}
+          height={128}
           className="mx-auto rounded-full w-32 h-32 mb-4"
         />
         <h1 className="text-4xl font-semibold mb-4">Hernando Abella</h1>
@@ -19,10 +19,10 @@ const Hero: React.FC = () => {
           I'm a dynamic Full-Stack engineer with a passion for crafting
           exceptional solutions. Let's redefine what's possible together!
         </p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300 mx-2">
           View Projects
         </button>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300 mx-2">
           Download CV
         </button>
       </div>
