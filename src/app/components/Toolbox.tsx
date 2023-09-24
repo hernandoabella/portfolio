9d3b474a8c16b55855b65c6b2b8407d5a9a8677b
Hernando Abella <hernandoabella@gmail.com>
2023-09-23 19:37:16 -0500

    Update Toolbox.tsx

--- src/app/components/Toolbox.tsx
+++ src/app/components/Toolbox.tsx
@@ -1,30 +1,88 @@
-import React from 'react';
-
+import React from "react";
+import Image from "next/image";
 
 // ... importa las demás imágenes ...
 
 const Toolbox: React.FC = () => {
   return (
-    <section className="bg-gray-100 py-10">
+    <section className="bg-white py-10">
       <div className="container mx-auto">
-        <h2 className="text-2xl font-semibold mb-4">Toolbox</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* HTML */}
-          <div className="bg-white p-4 rounded-lg shadow">
-            
-            <h3 className="text-lg font-semibold">HTML5</h3>
-            <p className="text-gray-600">Description of your HTML5 proficiency and any relevant details.</p>
+        <h2 className="text-center py-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight dark:text-slate-50">
+          My ToolBox 🧰
+        </h2>
+        <div className="flex align-center justify-center gap-6">
+          <div>
+            <Image
+              src="/html5-original.svg"
+              alt="html"
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
           </div>
-
-          {/* CSS */}
-          <div className="bg-white p-4 rounded-lg shadow">
-            
-            <h3 className="text-lg font-semibold">CSS3</h3>
-            <p className="text-gray-600">Description of your CSS3 proficiency and any relevant details.</p>
+          <div>
+            <Image
+              src="/css3-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
           </div>
-
-          {/* Otras habilidades/herramientas */}
-          {/* Sigue el mismo patrón para las demás habilidades/herramientas */}
+          <div>
+            <Image
+              src="/javascript-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/react-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/mongodb-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/nextjs-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/vercel.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/vercel.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
         </div>
       </div>
     </section>
