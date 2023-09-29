51dc797eb0eaf61c8dc0e4c3e3494bb480589ac8
Hernando Abella <hernandoabella@gmail.com>
2023-09-28 22:57:23 -0500

    Update MyWorks.tsx

--- src/app/components/MyWorks.tsx
+++ src/app/components/MyWorks.tsx
@@ -7,7 +7,6 @@ const MyWorks: React.FC = () => {
       <div className="container mx-auto">
         <h2 className="py-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight dark:text-slate-50">Showcase 🧑‍💼</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          
           <div className="bg-white p-4 rounded-lg shadow">
             <h3 className="text-lg font-semibold">Project 1</h3>
             <p className="text-gray-600">Description of your project. You can mention the technologies used, your role, and any relevant details. Include a link or button for more information.</p>
@@ -27,8 +26,6 @@ const MyWorks: React.FC = () => {
             <p className="text-gray-600">Description of your project. You can mention the technologies used, your role, and any relevant details. Include a link or button for more information.</p>
             <a href="#" className="mt-2 text-blue-500 hover:underline block">Learn More</a>
           </div>
-
-          {/* Agrega más proyectos según sea necesario */}
         </div>
       </div>
     </section>
