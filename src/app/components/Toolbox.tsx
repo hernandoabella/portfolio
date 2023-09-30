a60d87b2f0aef6b55df52de4fd729898dfa02f64
Hernando Abella <hernandoabella@gmail.com>
2023-09-29 23:55:33 -0500

    Update Toolbox.tsx

--- src/app/components/Toolbox.tsx
+++ src/app/components/Toolbox.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 
 const Toolbox: React.FC = () => {
   return (
-    <section className="bg-white py-10">
+    <section className="bg-white py-10 px-10">
       <div className="container mx-auto">
-        <h2 className="text-center py-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight dark:text-slate-50">
+        <h2 className="mb-4 text-center py-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight dark:text-slate-50">
           My ToolBox 🧰
         </h2>
-        <div className="flex align-center justify-center gap-6">
+        <div className="flex flex-wrap align-center justify-center gap-6">
           <div>
             <Image
               src="/html5-original.svg"
@@ -31,7 +31,43 @@ const Toolbox: React.FC = () => {
           </div>
           <div>
             <Image
+              src="/bootstrap-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/sass-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/tailwindcss-plain.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
               src="/javascript-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/typescript-original.svg"
               alt=""
               width={56}
               height={56}
@@ -49,7 +85,52 @@ const Toolbox: React.FC = () => {
           </div>
           <div>
             <Image
-              src="/mongodb-original.svg"
+              src="/angularjs-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/vuejs-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/express-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/gulp-plain.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/nodejs-original-wordmark.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/markdown-original.svg"
               alt=""
               width={56}
               height={56}
@@ -67,7 +148,7 @@ const Toolbox: React.FC = () => {
           </div>
           <div>
             <Image
-              src="/vercel.svg"
+              src="/jest-plain.svg"
               alt=""
               width={56}
               height={56}
@@ -76,7 +157,43 @@ const Toolbox: React.FC = () => {
           </div>
           <div>
             <Image
-              src="/vercel.svg"
+              src="/vercel-logotype-dark.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/github-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/docker-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/mongodb-original.svg"
+              alt=""
+              width={56}
+              height={56}
+              className="w-16 h-16"
+            />
+          </div>
+          <div>
+            <Image
+              src="/figma-original.svg"
               alt=""
               width={56}
               height={56}
