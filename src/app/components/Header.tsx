b87a240c4a5c5a809c9910972453ca00d27821ce
Hernando Abella <hernandoabella@gmail.com>
2023-10-10 16:28:02 -0500

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,7 @@
 // src/components/Header.tsx
 import React from "react";
+import { ThemeSwitcher } from "../ThemeSwitcher";
+
 
 const Header: React.FC = () => {
   return (
@@ -50,23 +52,8 @@ const Header: React.FC = () => {
             </ul>
           </nav>
           <div className="flex items-center border-l border-slate-200 ml-6 pl-6 dark:border-slate-800">
-            <label
-              className="sr-only"
-              id="headlessui-listbox-label-:R1jeilb6:"
-              data-headlessui-state=""
-            >
-              Theme
-            </label>
-            <button
-              type="button"
-              id="headlessui-listbox-button-:R2jeilb6:"
-              aria-haspopup="true"
-              aria-expanded="false"
-              data-headlessui-state=""
-              aria-labelledby="headlessui-listbox-label-:R1jeilb6: headlessui-listbox-button-:R2jeilb6:"
-            >
-              <i className="fa-sharp fa-solid fa-circle-half-stroke text-blue-500"></i>
-            </button>
+            <ThemeSwitcher />
+            
             <a
               href="https://x.com/hernandoabella"
               className="ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300"
