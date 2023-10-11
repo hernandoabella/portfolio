66282d5107a66c033f7498ce8105c1ee2299bbe0
Hernando Abella <hernandoabella@gmail.com>
2023-10-11 13:48:23 -0500

    Update Contact.tsx

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Contact: React.FC = () => {
   return (
-    <section id="contact" className="py-10 px-10">
+    <section id="contact" className="py-10 px-10 dark:bg-slate-900">
       <div className="container mx-auto">
-        <h2 className="py-4 text-3xl sm:text-4xl font-extrabold tracking-tight ">
+        <h2 className="pb-10 text-3xl sm:text-4xl font-extrabold tracking-tight">
           Contact Me
         </h2>
         <p className="text-lg mb-8">
@@ -13,18 +13,18 @@ const Contact: React.FC = () => {
         </p>
         <div>
           <div className="py-4 text-xl flex items-center gap-2">
-            <i className="fa-solid fa-phone"></i>+57-304-393-2597
+            <i className="fa-solid fa-phone dark:text-slate-500"></i>+57-304-393-2597
           </div>
           <div className="py-4 text-xl flex items-center gap-2">
-            <i className="fa-sharp fa-solid fa-envelopes"></i>
+            <i className="fa-sharp fa-solid fa-envelopes dark:text-slate-500"></i>
             hernandoabella@gmail.com
           </div>
           <div className="py-4 text-xl flex items-center gap-2">
-            <i className="fa-sharp fa-solid fa-location-dot"></i>Santa Marta,
+            <i className="fa-sharp fa-solid fa-location-dot dark:text-slate-500"></i>Santa Marta,
             Colombia
           </div>
           <div className="py-4 text-xl flex items-center gap-2">
-            <i className="fa-solid fa-clock"></i>9 am to 5 pm
+            <i className="fa-solid fa-clock dark:text-slate-500"></i>9 am to 5 pm
           </div>
         </div>
       </div>
