f1560f4b0e699a2a5510f70d6eabd183a756e488
Hernando Abella <hernandoabella@gmail.com>
2023-10-11 13:48:27 -0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="py-6">
+    <footer className="py-10 px-10 dark:bg-slate-900">
       <div className="container mx-auto text-center">
         <p>
           &copy; {new Date().getFullYear()} Hernando Abella. All rights
